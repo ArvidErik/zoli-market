9f62ace7687884114b53cef059300955b8ea3451
ArvidErik <erik.arvid.hosszu@gmail.com>
2024-01-28 20:12:00 +0100

    Rest Added

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -24,6 +24,16 @@ const CsvParser = require("json2csv").Parser;
       url:"https://www.erstemarket.hu/befektetesi_alapok/alap/LU1883849603",
       query: "span.i_10662_last_price",
       name: "AMUNDI FUNDS PIONEER US BOND A2 USD"
+    },
+    {
+      url:"https://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id=F00000O74P",
+      query: ".overviewKeyStatsTable tr:nth-child(2) td:nth-child(3)",
+      name: "Fidelity Funds - Global Dividend Fund A-Acc-USD"
+    },
+    {
+      url:"https://www.amundi.hu/lakossagi/product/view/LU1436216342",
+      query: ".table-bordered-vert tr:nth-child(3) td:nth-child(7) b",
+      name: "AMUNDI UNICREDIT PREMIUM PORTFOLIO - MULTI-ASSET - A (C) EUR"
     }
   ]
 
@@ -36,7 +46,15 @@ const CsvParser = require("json2csv").Parser;
       const $ = cheerio.load(page)
   
       $(options[i].query, page).each(function () {
-        const value = $(this).text()
+        let value = $(this).text()
+        if (options[i].url == "https://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id=F00000O74P") {
+          value = value.substring(value.length - 5)
+        }
+
+        if (options[i].url == "https://www.amundi.hu/lakossagi/product/view/LU1436216342") {
+          value = value.substring(0, 5)
+        }
+
         if (data.filter(el => el.value == value).length < 1) {
           data.push({name:options[i].name, value:value, url:options[i].url})
         }
@@ -46,6 +64,7 @@ const CsvParser = require("json2csv").Parser;
   }
 
 router.get("/", (req, res) => {
+  console.log(data);
  res.render('index.hbs', {data})
 });
 
